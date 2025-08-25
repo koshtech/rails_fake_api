c45f105bd320b4924f7907eca799365ccdec050e
Fernando Kosh <fernando.kosh@gmail.com>
2025-08-24 21:27:05 -0300

    Set CORS headers.

--- app/controllers/rails_fake_api/application_controller.rb
+++ app/controllers/rails_fake_api/application_controller.rb
@@ -1,11 +1,19 @@
 module RailsFakeApi
   class ApplicationController < ActionController::API
     skip_before_action :verify_authenticity_token, raise: false
+    before_action :set_cors_headers
 
     rescue_from StandardError, with: :internal_server_error
     rescue_from RailsFakeApi::Errors::ResourceNotFound, with: :resource_not_found_error
 
     private
+
+    def set_cors_headers
+      response.set_header('Access-Control-Allow-Origin', 'http://localhost:3020')
+      response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD')
+      response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
+      response.set_header('Access-Control-Max-Age', '1728000')
+    end
 
     def resource_not_found_error(exception)
       render json: { error: exception.message }, status: :not_found
